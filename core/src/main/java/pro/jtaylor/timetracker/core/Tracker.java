97498ba02da1817952f791ad7b26f4794e54ca10
Clyde Hill <clyde.hill@tolteq.com>
2015-10-13 17:22:46 -0500

    Replaced tabs with white spaces

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -11,29 +11,29 @@ public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
 
-	/** Add method
-	* @param TimeEntry to add to entries
-	**/
+    /** Add method
+    * @param TimeEntry to add to entries
+    **/
     public void add(TimeEntry entry) {
         entries.add(entry);
     }
 
-	/** Remove method
-	* @param TimeEntry to remove from entries
-	**/
+    /** Remove method
+    * @param TimeEntry to remove from entries
+    **/
     public void remove(TimeEntry entry) {
         entries.remove(entry);
     }
 
- 	/** Size method
-	**/
+    /** Size method
+    **/
 	public int size() {
         return entries.size();
     }
 	
-	/** get method
-	* @param int index for entries
-	**/
+    /** get method
+    * @param int index for entries
+    **/
     public TimeEntry get(int index) {
         return entries.get(index);
     }
