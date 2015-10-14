f88bb1abcbabdeae060b60a38cb1698be043c351
Clyde Hill <clyde.hill@tolteq.com>
2015-10-14 09:08:27 -0500

    another javadoc change

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -5,38 +5,46 @@ import org.springframework.stereotype.Component;
 import pro.jtaylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
-
+ 
+ /** 
+ * Variable.
+ * @Component.
+ */
 @Component
 public class Tracker {
-    @Autowired
+    /** 
+	* Variable.
+    * @Autowired.
+    */
+	@Autowired
     private List<TimeEntry> entries;
 
     /** 
-	* Add method
-    * @param TimeEntry entry to add to entries
+	* Add method.
+    * @param TimeEntry entry to add to entries.
     */
     public void add(TimeEntry entry) {
         entries.add(entry);
     }
 
     /** 
-	* Remove method
-    * @param TimeEntry to remove from entries
+	* Remove method.
+    * @param TimeEntry to remove from entries.
     */
     public void remove(TimeEntry entry) {
         entries.remove(entry);
     }
 
     /** 
-	* Size method
+	* Size method.
     */
     public int size() {
         return entries.size();
     }
 
     /** 
-	* get method
-    * @param int index for entries
+	* get method.
+    * @param int index for entries.
     */
     public TimeEntry get(int index) {
         return entries.get(index);
